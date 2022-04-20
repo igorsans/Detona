6c62c5b662557bc44b114b7e67753af93460a70c
igorsans <igor.santos0@hotmail.com>
2022-04-20 12:04:53 -0300

    *selecao personagens
    fix arq location

--- script/selecionePersonagem.js
+++ script/selecionePersonagem.js
@@ -1,9 +1,9 @@
 function selecioneRalph(){
-    window.location.href= "./personagens/escopo.html";
+    window.location.href= "../personagens/ralph/14098.html";
 }
 function selecioneFelix(){
-    window.location.href= "./personagens/felix.html";
+    window.location.href= "../personagens/felix/14577.html";
 }
 function selecioneUsuario(){
-    window.location.href= "./personagens/usuario.html";
+    window.location.href= "./personagens/user/usuario.html";
 }
