78115e2032736a6f159914ba6a49b4d8a411374b
igorsans <igor.santos0@hotmail.com>
2022-04-20 17:22:01 -0300

    ++User
    telas criadas para usuario jogar

--- script/selecionePersonagem.js
+++ script/selecionePersonagem.js
@@ -5,5 +5,5 @@ function selecioneFelix(){
     window.location.href= "../personagens/felix/14577.html";
 }
 function selecioneUsuario(){
-    window.location.href= "./personagens/user/usuario.html";
+    window.location.href= "../personagens/user/18714.html";
 }
