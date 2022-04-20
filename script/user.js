36a2bbd79d657b3e9bd27beee5029fda047c8525
igorsans <igor.santos0@hotmail.com>
2022-04-20 18:01:50 -0300

    +funcao
    funcao anterior otimizada

--- script/user.js
+++ script/user.js
@@ -1,24 +1,30 @@
 var idade = localStorage.getItem('idadeUser');
-var vidas = 4
+var vidasUser = 4
 
 
-function fase1(){
-    while (!false){
+function fase1() {
+    while (!false) {
         console.log(idade);
-        if (vidas < 1){
+        if (vidasUser < 1) {
             alert('Game Over\nInsira outra ficha e tente novamente');
             break;
         }
         console.log(idade);
         var resUsuario = prompt('O que você fará?');
-        if (resUsuario == 'b' && idade <=20){
+        if (resUsuario.toLowerCase() == 'b' && idade <= 20) {
             alert('foi jovem')
             return true
-        } else if (resUsuario == 'c' && idade > 20){
+        } else if (resUsuario.toLowerCase() == 'c' && idade > 20) {
             alert('foi senhor')
             return true
-        } alert('não foi')
-
-
+        } else if (resUsuario.toLowerCase() == 'a') {
+            vidasUser-- 
+            alert(`não foi\nVidas: ${vidasUser}`)
+        } else if ((resUsuario.toLocaleLowerCase() == 'c' && idade <= 20) || resUsuario.toLocaleLowerCase() == 'b' && idade > 20 ){
+            vidasUser--
+            alert(`não foi\nVidas: ${vidasUser}`)
+        } else {
+            alert(`Não entendi`)
+        }
     }
 }
